48514181011e64dc9d456ea26a1c042abc755866
Henry <tessygranados@Codigo-Mentor.local>
2020-05-15 23:17:28 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/api", (req , res) => {
+    res.json({
+        mensaje: "Nodejs and JWT"
+    });
+});
+
+app.post("/api/login", (req , res) => {
+    const user = {
+        id: 1,
+        nombre : "Henry",
+        email: "henry@email.com"
+    }
+
+    jwt.sign({user}, 'secretkey', {expiresIn: '32s'}, (err, token) => {
+        res.json({
+            token
+        });
+    });
+
+});
+
+app.post("/api/posts", verifyToken, (req , res) => {
+
+    jwt.verify(req.token, 'secretkey', (error, authData) => {
+        if(error){
+            res.sendStatus(403);
+        }else{
+            res.json({
+                    mensaje: "Post fue creado",
+                    authData
+                });
+        }
+    });
+});
+
+// Authorization: Bearer <token>
+function verifyToken(req, res, next){
+     const bearerHeader =  req.headers['authorization'];
+
+     if(typeof bearerHeader !== 'undefined'){
+          const bearerToken = bearerHeader.split(" ")[1];
+          req.token  = bearerToken;
+          next();
+     }else{
+         res.sendStatus(403);
+     }
+}
+
+app.listen(3000, () => {
+    console.log("nodejs app running...");
+});
